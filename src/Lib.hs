cc9f7bb9fdbdb65f1104f78c45a7b91b33ed7ce0
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-03 18:09:08 +0900

    add check of 0 than less

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,16 +4,17 @@ module Lib
     ) where
 
 
-fibonacci :: Int -> [Integer]
-fibonacci n | n == 0 = [0]
-  | n == 1 = [0, 1]
+fibonacci :: Int -> Maybe [Integer]
+fibonacci n | n == 0 = Just [0]
+  | n == 1 = Just [0, 1]
+  | n < 0 = Nothing
   | otherwise = 
     internalFibo [0, 1] 2 n
     where internalFibo = \dataList i end ->
              if i > end
                 then
                   case dataList of
-                    [] -> []
-                    _ -> dataList
+                    [] -> Nothing
+                    _ -> Just dataList
                 else
                  internalFibo (dataList ++ [(dataList !! (i - 1)) + (dataList !! (i - 2))]) (i + 1) end
