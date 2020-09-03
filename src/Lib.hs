79cef310d7bf2b5ad7483a5d443d927111a4ac3a
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-03 17:48:15 +0900

    revert

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,19 @@
 module Lib
-    ( someFunc,
+    ( 
       fibonacci
     ) where
 
--- import Data.Matrix
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
-
-{-
-fibonacci :: [Integer] -> Int -> Int -> [Integer]
-fibonacci dataList i end =
+fibonacci :: Int -> [Integer]
+fibonacci n | n == 0 = [0]
+  | n == 1 = [0, 1]
+  | otherwise = 
+    internalFibo [0, 1] 2 n
+    where internalFibo = \dataList i end ->
              if i > end
                 then
                   case dataList of
                     [] -> []
                     _ -> dataList
-                else
-                 fibonacci (dataList ++ [(dataList !! (i - 1)) + (dataList !! (i - 2))]) (i + 1) end
-
--}
-
-fibonacci :: Int -> Maybe [Integer]
-fibonacci n = 
-  internalFibo [0, 1] 2 n
-  where internalFibo = \dataList i end ->
-             if i > end
-                then
-                  case dataList of
-                    [] -> Nothing
-                    _ -> Just dataList
                 else
                  internalFibo (dataList ++ [(dataList !! (i - 1)) + (dataList !! (i - 2))]) (i + 1) end
